bb920c300b09e53a30f6b5df129b7044161a593b
Daniil <visomnibus@yandex.ru>
2023-07-18 15:22:50 +0300

    typo

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -5,7 +5,7 @@ function AuthForm({name}) {
   console.log(name);
   return (
     <div className="authorization">
-        <h2 className="authorization__title">{ name === "sign-up" ? "Регистрация" : "Войти" }</h2>
+        <h2 className="authorization__title">{ name === "sign-up" ? "Регистрация" : "Вход" }</h2>
         <form className="authorization__form">
             <input className="authorization__input authorization__input_type_email" placeholder="Email" type="text"></input>
             <input className="authorization__input authorization__input_type_password" placeholder="Пароль"></input>
