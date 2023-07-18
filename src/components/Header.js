6517842157c41df715f68462c10cce2ba3778015
Daniil <visomnibus@yandex.ru>
2023-07-18 21:53:09 +0300

    popup

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,17 @@ import React from "react";
 import logoImage from "../images/header-logo.svg";
 import { Link } from "react-router-dom";
 
-function Header({name}) {
+function Header({name, userData}) {
   console.log(name);
   return (
-    <header className={name === 'sign-up' || name === 'sign-in' ? "header header_login" : "header"}>
+    <header className={"header header_login"}>
       <img className="header__logo" src={logoImage} alt="Лого сайта" />
-      {name === 'sign-up' && <Link className={"header__link"} to="/sign-in">Войти</Link>}
-      {name === 'sign-in' && <Link className={"header__link"} to="/sign-up">Регистрация</Link>}
+      { name === 'sign-up' && <Link className={"header__link"} to="/sign-in">Войти</Link> }
+      { name === 'sign-in' && <Link className={"header__link"} to="/sign-up">Регистрация</Link> }
+      { name === 'main' && <div style={{display: "flex"}}>
+        <p className="header__user-email">tutbudetemail@yandex.ru</p>
+        && <Link className={"header__link"} to="/sign-in">Выйти</Link>
+      </div> }
       </header>
   );
 }
